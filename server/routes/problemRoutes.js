25968a604c1bdc12d91963334301ec61c8ce2e39
divyy101 <divyanshsingh74178@gmail.com>
2026-07-15 15:22:44 +0530

    feat: add LeetCode sync functionality

--- server/routes/problemRoutes.js
+++ server/routes/problemRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createProblem, getAllProblems, updateProblem, deleteProblem } = require("../controllers/problemController");
+const { createProblem, getAllProblems, updateProblem, deleteProblem, syncLeetCode } = require("../controllers/problemController");
 
 router.post("/",      createProblem);
 router.get("/",       getAllProblems);
+router.post("/sync-leetcode", syncLeetCode);
 router.patch("/:id",  updateProblem);
 router.delete("/:id", deleteProblem);
 
